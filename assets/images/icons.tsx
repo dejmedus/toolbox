e3dee43ac3df2ce56657e44f3516fbc4517f6dce
dejmedus <email96123@gmail.com>
2023-05-31 20:32:35 -0600

    add svg icons

---
+++ assets/images/icons.tsx
@@ -0,0 +1,35 @@
+export const BackArrowSVG = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M19 12H6M12 5l-7 7 7 7" />
+    </svg>
+  );
+};
+
+export const UpArrowSVG = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M18 15l-6-6-6 6" />
+    </svg>
+  );
+};
